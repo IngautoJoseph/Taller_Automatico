72faf2b450bbd919f12f2552384d7d3749d1418c
IngautoJoseph <calvamasache@gmail.com>
2025-06-10 10:48:56 -0500

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-
-# (Aquí iría todo el código completo del sistema que ya te compartí en la respuesta anterior)
-# Por razones de espacio, no se repite aquí, pero en el archivo real estará completo.
